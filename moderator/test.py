44f9ad49f74221f787294eada7a10943f3ffeb65
RoJen-200 <deviastery@gmail.com>
2025-11-16 16:37:51 +0300

    feat:  improve model accuracy

--- moderator/test.py
+++ moderator/test.py
@@ -1,28 +1,28 @@
 from . import moderate, add_review, retrain_model
 
-# # Модерирование отзыва
-# review = "Окей"
-# result = moderate(review) # Возвращает уровни от 0 до 5
-# print(result) # Output: {'insult': 0, 'threat': 0, 'obscenity': 0, 'meaningless': 5}
-# if not result:
-#     print("Произошла ошибка при модерации отзыва")
+# Модерирование отзыва
+review = "Окей"
+result = moderate(review) # Возвращает уровни от 0 до 5
+print(result) # Output: {'insult': 0, 'threat': 0, 'obscenity': 0, 'meaningless': 5}
+if not result:
+    print("Произошла ошибка при модерации отзыва")
 
-# review = "Окей"
-# result = moderate(review, True) # Возвращает вероятности
-# print(result) # Output: {'insult': 0.027452949434518814, 'threat': 0.03570876270532608, 'obscenity': 0.14793378114700317, 'meaningless': 0.7804442644119263}
-# if not result:
-#     print("Произошла ошибка при модерации отзыва")
+review = "Окей"
+result = moderate(review, True) # Возвращает вероятности
+print(result) # Output: {'insult': 0.027452949434518814, 'threat': 0.03570876270532608, 'obscenity': 0.14793378114700317, 'meaningless': 0.7804442644119263}
+if not result:
+    print("Произошла ошибка при модерации отзыва")
 
-# # Добавление отзыва в датасет
-# # Нужно указать один или несколько классов (normal, insult, threat, obscenity, meaningless)
-# # Также можно писать отзывы вручную в файл added_dataset_reviews.csv
-# review = "Убью"
-# classes = ["threat", "meaningless"]
-# success = add_review(review, classes)
-# if not success:
-#     print("Произошла ошибка при добавлении отзыва")
+# Добавление отзыва в датасет
+# Нужно указать один или несколько классов (normal, insult, threat, obscenity, meaningless)
+# Также можно писать отзывы вручную в файл added_dataset_reviews.csv
+review = "Побью"
+classes = ["threat", "meaningless"]
+success = add_review(review, classes)
+if not success:
+    print("Произошла ошибка при добавлении отзыва")
 
-# После добавления отзывов необходимо запустить переобучение модели (это может занять некоторое время)
+# После добавления отзывов необходимо запустить переобучение модели (это займет некоторое время)
 # Обучение модели
 success = retrain_model(
     epochs=50,              # количество эпох (по умолчанию 50)
